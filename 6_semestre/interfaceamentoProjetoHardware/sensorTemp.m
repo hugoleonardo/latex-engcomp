c13d336cd98fc359320ea81dbb9c68831a651b02
Danilo Henrique Costa Souza <dhcsouza@gmail.com>
2012-12-10 22:28:55 -0300

    Signed-off-by: Danilo Henrique Costa Souza <dhcsouza@gmail.com>

---
+++ 6_semestre/interfaceamentoProjetoHardware/sensorTemp.m
@@ -0,0 +1,12 @@
+% Gerar o sinal a partir da curva do termistor
+% amplificar o sinal e normalizar
+    % 1.Calcular o número de níveis que será utilizado (antes da amplificação
+    %   do sinal)
+    % 2.Encontrar o número de níveis por ºC
+    % 3.Encontrar o ganho do sinal necessário para amplificação na faixa
+    %   entre 0.5 e 4.5 volts (0.5 volts de cada lado como margem de
+    %   segurança)
+    % 4.Encontrar o offset do sinal
+    % 5. Definir o valor de Rf
+    % 6. Encontrar o valor de Rh (pela equação do offset)
+    % 7. Encontrar o valor de RL (pela equação do ganho)
