e01201b2ba937e101a4e2e37fcb040e5f7c202a1
Hugo Santos <hugoleonardoeng07@gmail.com>
2013-02-05 18:16:19 -0300

    Borda de crescimento do sinal limitada em PHI

--- 6_semestre/phi/projetoFinal/code/projetoFinal.m
+++ 6_semestre/phi/projetoFinal/code/projetoFinal.m
@@ -13,6 +13,24 @@ v_min_ad = .5;
 vref_ad = 5;
 n_bits_ad = 1;
 rf=100000;
+duracao = .8; %duracao em segundos
+qt_amostras=duracao*1000;
+r=0.05; %raio em metros
+v=5; %velocidade em m/s
+t=2*pi*r/v;
+
+phi = pi/16; %angulo de abertura
+t_phi = phi*r/v; %tempo da luz na abertura
+n_amostras_t_phi = round(t_phi*1000);
+n_amostras_low = round(t*1000 - n_amostras_t_phi - 18);
+
+n_amostras_transicao = 9;
+n_amostras_high = round(t_phi*1000)-n_amostras_transicao*2;
+
+if(round(t_phi*1000) < 19)
+    n_amostras_transicao = round(t_phi/2*1000);
+    n_amostras_high = 0;
+end
 
 %######## LUMINANCIA MAXIMA E MINIMA REAL E IDEAL ########
 illum_max_ideal = 100; %luminancia maxima real
@@ -58,49 +76,58 @@ rh = (vref*rf)/offset;
 %calculo do RL
 rl = rf/((ganho - 1) - rf/rh);
 
-qt_amostras=252;
-
-
+%######## GERANDO SINAL REAL E IDEAL ########
 i=1;
-tmp1 = v1_illum_min;
 while i<qt_amostras
     tmp1 = v1_illum_min;
     tmp2 = v1_illum_max;
     v0_sinal(i) = tmp1;
-    sinal(i) = v1_illum_min_ideal;
-    for j=1:8,
-        tmp1 = tmp1 + 0.074;
+    v0_sinal_ideal(i) = v1_illum_min_ideal;
+    for j=1:n_amostras_transicao,
+        %if (j>int64(t_phi/2)) break; end
+        tmp1 = tmp1 + 0.070;
         v0_sinal(i+j) = tmp1;
-        sinal(i+j) = v1_illum_max_ideal;
+        v0_sinal_ideal(i+j) = v1_illum_max_ideal;
+        if ((i+j)==qt_amostras) break; end
     end
     i=(i+j);
-    v0_sinal(i+1) = tmp1+0.037;
-    sinal(i+1) = v1_illum_max_ideal;
-    i=i+1;
-    for l=1:32,
+    if (i==qt_amostras) break; end
+    %v0_sinal(i+1) = tmp1+0.037;
+    %v0_sinal_ideal(i+1) = v1_illum_max_ideal;
+    %i=i+1;
+    %if (i==qt_amostras) break; end
+    l=1;
+    while l<n_amostras_high+1,
+        %if(t_phi<=18) break; end
         v0_sinal(i+l) = v1_illum_max;
-        sinal(i+l) = v1_illum_max_ideal;
+        v0_sinal_ideal(i+l) = v1_illum_max_ideal;
+        l=l+1;
+        if ((i+l)==qt_amostras) break; end
     end
-    i=i+l;
-    tmp2 = tmp2 - 0.037;
-    v0_sinal(i+1) = tmp2;
-    sinal(i+1) = v1_illum_min_ideal;
-    for m=1:8,
-        tmp2 = tmp2 - 0.074;
-        v0_sinal(i+m) = tmp2;
-        sinal(i+m) = v1_illum_min_ideal;
+    i=i+l-1;
+    if (i==qt_amostras) break; end
+    %tmp2 = tmp2 - 0.037;
+    %v0_sinal(i+1) = tmp2;
+    %v0_sinal_ideal(i+1) = v1_illum_min_ideal;
+    for m=1:n_amostras_transicao,
+        %if (m>t_phi/2) break; end
+        tmp1 = tmp1 - 0.070;
+        v0_sinal(i+m) = tmp1;
+        v0_sinal_ideal(i+m) = v1_illum_min_ideal;
+        if ((i+m)==qt_amostras) break; end
     end
     i=i+m;
-    for i=i+1:i+64,
-        v0_sinal(i) = tmp2;
-        sinal(i) = v1_illum_min_ideal;
+    if (i==qt_amostras) break; end
+    for i=i+1:i+n_amostras_low,
+        v0_sinal(i) = v1_illum_min;
+        v0_sinal_ideal(i) = v1_illum_min_ideal;
+        if (i==qt_amostras) break; end
     end
 end
 
-%intervalo_temp = 0 : 1 : 295-1;
-intervalo_temp = 0 : 1 : 340-1;
-plot(intervalo_temp, v0_sinal),hold,plot(intervalo_temp, sinal,'r');
-title('Responsividade real(azul e 70% menor) e ideal(vermelho) do sensor'); xlabel('Tempo(ms)'); ylabel('Tensão(V)');
+intervalo_temp = 0 : 1 : qt_amostras-1;
+plot(intervalo_temp, v0_sinal),hold,plot(intervalo_temp, v0_sinal_ideal,'r');
+%title('Responsividade real(azul e 70% menor) e ideal(vermelho) do sensor'); xlabel('Tempo(ms)'); ylabel('Tensão(V)');
 %plot(intervalo_temp, v0_sinal);
 %hold;
 %subplot(1,2,2),plot(intervalo_temp, sinal);
