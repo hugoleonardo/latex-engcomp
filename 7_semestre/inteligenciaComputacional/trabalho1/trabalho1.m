507063e6aa99866ea73db6b04b016b3b26f53f60
Hugo Santos <hugoleonardoeng07@gmail.com>
2013-05-04 19:57:50 -0300

    Inicio trabalho de IA

---
+++ 7_semestre/inteligenciaComputacional/trabalho1/trabalho1.m
@@ -0,0 +1,23 @@
+%AG Inteligencia Artificial
+
+%Criterio de avaliacao
+mat(9,8) = 0;
+lin1 = [3];
+lin2 = [2,1];
+lin3 = [3,2];
+lin4 = [2,2];
+lin5 = [6];
+lin6 = [1,5];
+lin7 = [6];
+lin8 = [1];
+lin9 = [2];
+col1 = [1,2];
+col2 = [3,1];
+col3 = [1,5];
+col4 = [7,1];
+col5 = [5];
+col6 = [3];
+col7 = [4];
+col8 = [1,2];
+
+%Populacao Inicial
