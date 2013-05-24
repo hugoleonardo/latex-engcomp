5d68b27f14b2f87260fd45ee39e8468f11c109e1
Danilo Souza <dhcsouza@gmail.com>
2013-05-24 18:35:12 -0300

    Projeto de Pesquisa
    Empreendedorismo e Plano de Negócios
    Apresentação (latex)
    Figuras
    Mapa mental da aplicação

---
+++ 7_semestre/empreendedorismo/danilo/projetoPesquisa.mm
@@ -0,0 +1,12 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1369185115109" ID="ID_1417661673" MODIFIED="1369185419722" TEXT="Otmizar vendas de A&#xe7;a&#xed;">
+<node CREATED="1369185162387" ID="ID_1499882652" MODIFIED="1369185252955" POSITION="right" TEXT="Configurar par&#xe2;metros">
+<node CREATED="1369185260593" ID="ID_1570179994" MODIFIED="1369185275348" TEXT="Tamanho da por&#xe7;&#xe3;o"/>
+<node CREATED="1369185280064" ID="ID_1185624250" MODIFIED="1369185286973" TEXT="Tipo de a&#xe7;a&#xed;"/>
+<node CREATED="1369185287461" ID="ID_250615731" MODIFIED="1369185298090" TEXT="Quantidade de por&#xe7;&#xf5;es"/>
+<node CREATED="1369185300337" ID="ID_1938143340" MODIFIED="1369185308317" TEXT="Tempo entre por&#xe7;&#xf5;es (opcional)"/>
+</node>
+<node CREATED="1369185421024" ID="ID_162573883" MODIFIED="1369187940622" POSITION="left" TEXT="Reduzir o n&#xfa;mero de reposi&#xe7;&#xf5;es (&#xe1;gua e a&#xe7;a&#xed;) por expediente"/>
+</node>
+</map>
