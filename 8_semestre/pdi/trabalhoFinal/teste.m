21b529010a448cdb2665db534519580949810e1a
Danilo Souza <dhcsouza@gmail.com>
2014-12-10 18:53:26 -0200

    PDI - final
    teste

--- 8_semestre/pdi/trabalhoFinal/teste.m
+++ 8_semestre/pdi/trabalhoFinal/teste.m
@@ -1,12 +1,12 @@
 
 clear;clc;
-p = ones(4);
+p = ones(2)
 p1=0;
 %p1 = zeros(4,1);
-p2(1,8) = 0;
+p2(1,4) = 0;
 
-for i=1:4
-    for j=1:4
+for i=1:2
+    for j=1:2
         p1 = p(i,j) + p1
         p2(1,i+j) = p1
     end
